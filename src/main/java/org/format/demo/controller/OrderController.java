3c9bd140c52ad0741a4c2a18fe96b113b6b6f235
Format <fangjian0423@gmail.com>
2014-11-28 11:26:41 +0800

    新增的权限mode加入测试用例

--- src/main/java/org/format/demo/controller/OrderController.java
+++ src/main/java/org/format/demo/controller/OrderController.java
@@ -2,6 +2,7 @@ package org.format.demo.controller;
 
 
 import org.format.demo.annotation.Authorization;
+import org.format.demo.model.AuthMode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,12 @@ public class OrderController {
     @RequestMapping
     @Authorization(roles = {"manager"})
     public String index() {
+        return "user";
+    }
+
+    @RequestMapping("/add")
+    @Authorization(auth = {"add-user", "add-order"}, mode = AuthMode.OR)
+    public String add() {
         return "user";
     }
 
