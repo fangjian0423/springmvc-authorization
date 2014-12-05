b896663bd4751fab2b657e416d86e05bec98b936
format <fangjian0423@gmail.com>
2014-12-06 00:09:50 +0800

    接口加上注释

--- src/main/java/org/format/demo/handler/AuthHandler.java
+++ src/main/java/org/format/demo/handler/AuthHandler.java
@@ -7,10 +7,24 @@ import java.util.Set;
 
 public interface AuthHandler {
 
+    /**
+     * 权限验证逻辑
+     * @param userName
+     * @param auth
+     * @param roles
+     * @param mode
+     * @return
+     */
     public boolean handleAuth(String userName, Set<String> auth, Set<String> roles, AuthMode mode);
 
+    /**
+     * 权限验证成功后的操作
+     */
     public void authSuccess();
 
+    /**
+     * 权限验证失败后的操作
+     */
     public void authError();
 
 }
